bee97fc666f77ca898ae5de815122b934315a589
iamsstef <115187950+iamsstef@users.noreply.github.com>
2024-06-08 02:51:14 +0200

    Home Assistant integration and automatic discovery - intercom_mqtt.ino
    Added Home Assistant integration with automatic discovery, controls and ring events.

--- intercom_mqtt.ino
+++ intercom_mqtt.ino
@@ -30,32 +30,149 @@ bool reconnectMqtt = false;
 uint32_t lastReconnect = 0;
 uint32_t lastMillis = 0;
 uint32_t millisDisconnect = 0;
+bool autounlock = 0;
+uint8_t MUTEPIN_JMP_STATE;
 
 BasicBlinker basicBlinker(500);
 BasicTimer constructedTimer;
 BasicTimer delayTimer;
 BasicTimer BellTimer(2000);
 
-int IOLOCK = 1;
-int MUTEPIN_JMP = 2;
-int IOBELL = 3;
-int MUTEPIN = 0;
-int IORST = 10;
-int IOLED = 8;
-int call_url_length = 400;
-
-bool autounlock;
-
-String call_url;
-String struid;
-String strvolt;
-
-uint32_t chipID;
-
-String APSSID;
-const char *APPWD = "password";
-
+const int IOLOCK = 1;
+const int MUTEPIN_JMP = 2;
+const int IOBELL = 3;
+const int MUTEPIN = 0;
+const int IORST = 10;
+const int IOLED = 8;
 const int led = IOLED;
+
+uint64_t macAddress = ESP.getEfuseMac();
+uint64_t macAddressTrunc = macAddress << 40;
+uint32_t chipID = macAddressTrunc >> 40;
+
+String uid = String(chipID, HEX);
+String friendly_name = "Intercom";
+String APSSID = friendly_name + "-" + uid;
+const char * APPWD = "password";
+
+String discovery_prefix = "homeassistant";
+
+String will_pubtopic = "devices/" + uid + "/status";
+String mute_pubtopic = "devices/" + uid + "/mute";
+String autounlock_pubtopic = "devices/" + uid + "/autounlock";
+String ring_pubtopic = "devices/" + uid + "/events";
+
+String birth_subtopic = discovery_prefix + "/status";
+String cmd_subtopic = "devices/" + uid + "/cmd";
+String mute_subtopic = "devices/" + uid + "/cmd/mute";
+String autounlock_subtopic = "devices/" + uid + "/cmd/autounlock";
+
+const char * configtopic(String compoment, String identifier) {
+  return String(discovery_prefix + "/" + compoment + "/" + identifier + uid + "/config").c_str();
+}
+
+String event_payload =
+"{"
+" \"name\": \"Intercom RING Event\","
+" \"state_topic\": \"" + ring_pubtopic + "\","
+" \"event_types\": [\"ring\"],"
+" \"qos\": 0,"
+" \"availability_topic\": \"" + will_pubtopic + "\","
+" \"device_class\": \"doorbell\","
+" \"icon\": \"mdi:doorbell\","
+" \"unique_id\": \"ringevent_" + uid + "\","
+" \"device\": {"
+"   \"identifiers\": [\"" + uid + "\"],"
+"   \"hw_version\": \"" + String(ESP.getChipModel()) + "\","
+"   \"manufacturer\": \"Innovutech\","
+"   \"model\": \"intercom_proto\","
+"   \"name\": \"Intercom - " + uid + "\","
+"   \"serial_number\": \"" + uid + "\","
+"   \"sw_version\": \"1.0.0\" "
+" }"
+"}"
+;
+
+String unlockbtn_payload =
+"{"
+" \"name\": \"Unlock\","
+" \"command_topic\": \"" + cmd_subtopic + "\","
+" \"payload_press\": \"unlock\","
+" \"qos\": 0,"
+" \"availability_topic\": \"" + will_pubtopic + "\","
+" \"icon\": \"mdi:lock-open\","
+" \"unique_id\": \"unlockbtn_" + uid + "\","
+" \"device\": {"
+"   \"identifiers\": [\"" + uid + "\"]"
+" }"
+"}"
+;
+
+String rbbtn_payload =
+"{"
+" \"name\": \"Reboot\","
+" \"command_topic\": \"" + cmd_subtopic + "\","
+" \"payload_press\": \"reboot\","
+" \"qos\": 0,"
+" \"availability_topic\": \"" + will_pubtopic + "\","
+" \"device_class\": \"restart\","
+" \"icon\": \"mdi:restart\","
+" \"unique_id\": \"rbbtn_" + uid + "\","
+" \"device\": {"
+"   \"identifiers\": [\"" + uid + "\"]"
+" }"
+"}"
+;
+
+String rstbtn_payload =
+"{"
+" \"name\": \"Reset Setings\","
+" \"command_topic\": \"" + cmd_subtopic + "\","
+" \"payload_press\": \"reset\","
+" \"qos\": 0,"
+" \"availability_topic\": \"" + will_pubtopic + "\","
+" \"device_class\": \"restart\","
+" \"icon\": \"mdi:cog-refresh\","
+" \"unique_id\": \"rstbtn_" + uid + "\","
+" \"device\": {"
+"   \"identifiers\": [\"" + uid + "\"]"
+" }"
+"}"
+;
+
+String mutesw_payload =
+"{"
+" \"name\": \"Sound\","
+" \"command_topic\": \"" + mute_subtopic + "\","
+" \"payload_off\": \"1\","
+" \"payload_on\": \"0\","
+" \"state_topic\": \"" + mute_pubtopic + "\","
+" \"qos\": 0,"
+" \"availability_topic\": \"" + will_pubtopic + "\","
+" \"icon\": \"mdi:volume-high\","
+" \"unique_id\": \"mutesw_" + uid + "\","
+" \"device\": {"
+"   \"identifiers\": [\"" + uid + "\"]"
+" }"
+"}"
+;
+
+String autounlocksw_payload =
+"{"
+" \"name\": \"Autounlock\","
+" \"command_topic\": \"" + autounlock_subtopic + "\","
+" \"payload_off\": \"0\","
+" \"payload_on\": \"1\","
+" \"state_topic\": \"" + autounlock_pubtopic + "\","
+" \"qos\": 0,"
+" \"availability_topic\": \"" + will_pubtopic + "\","
+" \"icon\": \"mdi:lock-open-check\","
+" \"unique_id\": \"autounlocksw_" + uid + "\","
+" \"device\": {"
+"   \"identifiers\": [\"" + uid + "\"]"
+" }"
+"}"
+;
 
 void setup() {
   Serial.begin(115200);
@@ -67,25 +184,6 @@ void setup() {
     return;
   }
 
-  #ifdef ESP8266
-  
-    chipID = ESP.getChipId();
-  
-  #endif
-
-  #ifdef ESP32
-  
-    uint64_t macAddress = ESP.getEfuseMac();
-    uint64_t macAddressTrunc = macAddress << 40;
-    chipID = macAddressTrunc >> 40;
-  
-  #endif
-
-  struid = String(chipID, HEX);
-  strvolt = "N/A";
-
-  APSSID = "Intercom-" + struid;
-
   pinMode(IOLOCK, OUTPUT);
   pinMode(IOLED, OUTPUT);
   pinMode(IOBELL, INPUT);
@@ -93,7 +191,7 @@ void setup() {
   pinMode(IORST, INPUT_PULLUP);
   pinMode(MUTEPIN_JMP, INPUT_PULLUP);
 
-  uint8_t MUTEPIN_JMP_STATE = !digitalRead(MUTEPIN_JMP);
+  MUTEPIN_JMP_STATE = !digitalRead(MUTEPIN_JMP);
 
   digitalWrite(IOLOCK, LOW);
   digitalWrite(MUTEPIN, MUTEPIN_JMP_STATE);
@@ -165,6 +263,7 @@ void setup() {
   mqttClient.onUnsubscribe(onMqttUnsubscribe);
   mqttClient.onMessage(onMqttMessage);
   mqttClient.onPublish(onMqttPublish);
+  mqttClient.setWill(will_pubtopic.c_str(), 0, false, "offline");
   mqttClient.setCleanSession(true);
   mqttClient.setCACert(rootca);
   mqttClient.setCredentials(mqttusr, mqttpwd);
@@ -250,24 +349,28 @@ void onMqttConnect(bool sessionPresent) {
   Serial.println("Connected to MQTT.");
   Serial.print("Session present: ");
   Serial.println(sessionPresent);
-  
-  String unlock_subtopic = "devices/" + APSSID + "/cmd";
-  String mute_subtopic = "devices/" + APSSID + "/cmd/mute";
-  String autounlock_subtopic = "devices/" + APSSID + "/cmd/autounlock";
 
-  String pubtopic = "devices/announce";
-
-  uint16_t packetIdSub0 = mqttClient.subscribe(unlock_subtopic.c_str(), 0);
+  uint16_t packetIdSub0 = mqttClient.subscribe(cmd_subtopic.c_str(), 0);
   uint16_t packetIdSub1 = mqttClient.subscribe(mute_subtopic.c_str(), 0);
   uint16_t packetIdSub2 = mqttClient.subscribe(autounlock_subtopic.c_str(), 0);
+  uint16_t packetIdSub3 = mqttClient.subscribe(birth_subtopic.c_str(), 0);
   Serial.print("Subscribing at QoS 0, packetId: ");
   Serial.println(packetIdSub0);
   Serial.println(packetIdSub1);
   Serial.println(packetIdSub2);
 
-  uint16_t packetIdPub0 = mqttClient.publish(pubtopic.c_str(), 0, false, String("{\"name\": \"" + APSSID + "\", \"status\": \"ONLINE\"}").c_str());
+  uint16_t packetIdPub0 = mqttClient.publish(will_pubtopic.c_str(), 0, false, "online");
+  udelay(200);
+  uint16_t packetIdPub1 = mqttClient.publish(autounlock_pubtopic.c_str(), 0, true, String(autounlock).c_str());
+  udelay(200);
+  uint16_t packetIdPub2 = mqttClient.publish(mute_pubtopic.c_str(), 0, true, String(!MUTEPIN_JMP_STATE).c_str());
+  udelay(200);
   Serial.println("Publishing at QoS 0, packetId: ");
   Serial.println(packetIdPub0);
+  Serial.println(packetIdPub1);
+  Serial.println(packetIdPub2);
+  udelay(200);
+  publish_discovery();
 }
 
 void onMqttDisconnect(espMqttClientTypes::DisconnectReason reason) {
@@ -290,12 +393,6 @@ void onMqttUnsubscribe(uint16_t packetId) {
 }
 
 void onMqttMessage(const espMqttClientTypes::MessageProperties& properties, const char* topic, const uint8_t* payload, size_t len, size_t index, size_t total) {
-  String cmd_topic = "devices/" + APSSID + "/cmd";
-  String mute_topic = "devices/" + APSSID + "/cmd/mute";
-  String autounlock_topic = "devices/" + APSSID + "/cmd/autounlock";
-
-  String mute_pubtopic = "devices/" + APSSID + "/mute";
-  String autounlock_pubtopic = "devices/" + APSSID + "/autounlock";
 
   char* tmp = new char[len + 1];
   memcpy(tmp, payload, len);
@@ -303,13 +400,15 @@ void onMqttMessage(const espMqttClientTypes::MessageProperties& properties, cons
   String data = String(tmp);
   delete[] tmp;
 
-  if (String(topic) == cmd_topic) {
+  if (String(topic) == cmd_subtopic) {
     if (data == "unlock"){
       unlockDoor();
     } else if (data == "reset") {
       handleReset();
+    } else if (data == "reboot") {
+      handleReboot();
     }
-  } else if (String(topic) == mute_topic) {
+  } else if (String(topic) == mute_subtopic) {
     int mapped_setting = (data == "1") ? 1 : ((data == "0") ? 0 : -1);
 
     switch (mapped_setting) {
@@ -327,7 +426,7 @@ void onMqttMessage(const espMqttClientTypes::MessageProperties& properties, cons
         Serial.print("Unrecognised option -> ");
         Serial.println(data);
     }
-  } else if (String(topic) == autounlock_topic) {
+  } else if (String(topic) == autounlock_subtopic) {
     int mapped_setting = (data == "1") ? 1 : ((data == "0") ? 0 : -1);
 
     switch (mapped_setting) {
@@ -343,6 +442,11 @@ void onMqttMessage(const espMqttClientTypes::MessageProperties& properties, cons
       default:
         Serial.print("Unrecognised option -> ");
         Serial.println(data);
+    }
+  } else if (String(topic) == birth_subtopic) {
+    if (data == "online") {
+      udelay(200);
+      publish_discovery();
     }
   }
 
@@ -368,12 +472,47 @@ void networkingTask() {
   }
 }
 
+void publish_discovery() {
+  //mqtt-discovery for automatic discovery of the device on Home Assistant
+  //note for myself: this should be called durring the mqtt connection event or when the device receives a birth status message from homeassistant's birth topic
+  Serial.println("Publishing at QoS 0, packetId: ");
+  uint16_t packetIdPub0 = mqttClient.publish(configtopic("event", "ring"), 0, false, event_payload.c_str());
+  udelay(200);
+  uint16_t packetIdPub1 = mqttClient.publish(configtopic("button", "unlock"), 0, false, unlockbtn_payload.c_str());
+  udelay(200);
+  uint16_t packetIdPub2 = mqttClient.publish(configtopic("switch", "mute"), 0, false, mutesw_payload.c_str());
+  udelay(200);
+  uint16_t packetIdPub3 = mqttClient.publish(configtopic("switch", "autounlock"), 0, false, autounlocksw_payload.c_str());
+  udelay(200);
+  uint16_t packetIdPub4 = mqttClient.publish(configtopic("button", "rb"), 0, false, rbbtn_payload.c_str());
+  udelay(200);
+  uint16_t packetIdPub5 = mqttClient.publish(configtopic("button", "rst"), 0, false, rstbtn_payload.c_str());
+  udelay(200);
+
+  Serial.println(packetIdPub0);
+  Serial.println(packetIdPub1);
+  Serial.println(packetIdPub2);
+  Serial.println(packetIdPub3);
+  Serial.println(packetIdPub4);
+  Serial.println(packetIdPub5);
+}
+
 void handleReset() {
   digitalWrite(led, 0);
   udelay(500);
   digitalWrite(led, 1);
 
+  mqttClient.disconnect(true);
   wm.resetSettings();
+  ESP.restart();
+}
+
+void handleReboot() {
+  digitalWrite(led, 0);
+  udelay(500);
+  digitalWrite(led, 1);
+
+  mqttClient.disconnect(true);
   ESP.restart();
 }
 
@@ -490,9 +629,13 @@ void loop() {
 }
 
 void publishRing() {
-  String ring_pubtopic = "devices/" + APSSID + "/events";
   Serial.println("Bell ring detected!");
-  mqttClient.publish(ring_pubtopic.c_str(), 0, true, "RING");
+  const char * payload =
+  "{"
+  "\"event_type\": \"ring\""
+  "}"
+  ;
+  mqttClient.publish(ring_pubtopic.c_str(), 0, false, payload);
 }
 
 void udelay(int ms) {
